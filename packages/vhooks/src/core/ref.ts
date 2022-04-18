27f6afa353af82f72ceec9086c37fbb6edda0577
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-04-18 14:20:27 +0800

    feat: refactor useRef

--- packages/vhooks/src/core/ref.ts
+++ packages/vhooks/src/core/ref.ts
@@ -1,12 +1,16 @@
-import { customRef } from 'vue-demi'
+import type { Ref } from 'vue-demi'
+import { customRef, isRef, ref, unref } from 'vue-demi'
+import type { MaybeRef } from '@m9ch/vhooks-types'
 
-export const useRef = <T>(value: T) => customRef<T>((track, trigger) => ({
-  get: () => {
-    track()
-    return value
-  },
-  set: (newVal) => {
-    value = newVal
-    trigger()
-  },
-}))
+export const useRef = <T>(value?: MaybeRef<T>): Ref<T> => isRef(value)
+  ? ref(unref(value)) as Ref<T>
+  : customRef<T>((track, trigger) => ({
+    get: () => {
+      track()
+      return unref(value)
+    },
+    set: (newVal) => {
+      value = newVal
+      trigger()
+    },
+  }))
