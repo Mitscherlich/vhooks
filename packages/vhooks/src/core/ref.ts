4bbd50c9e94f25246576606cb32143b3bf5f5175
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-04-26 15:53:05 +0800

    feat(ref): refactor `useRef` with `useMemo`

--- packages/vhooks/src/core/ref.ts
+++ packages/vhooks/src/core/ref.ts
@@ -1,19 +1,9 @@
 import type { Ref } from 'vue-demi'
-import { customRef, isRef, ref } from 'vue-demi'
+import { isRef } from 'vue-demi'
 import type { MaybeRef } from '@m9ch/vhooks-types'
+import { useMemo } from './memo'
 
 /**
- * An alias version of Vue.ref
+ * similar to `React.useRef`, but returns `Vue.ref`
  */
-export const useRef = <T>(value?: MaybeRef<T>): Ref<T> => isRef<T>(value)
-  ? ref<Ref<T>>(value)
-  : customRef<T>((track, trigger) => ({
-    get: () => {
-      track()
-      return value as T
-    },
-    set: (newVal) => {
-      value = newVal
-      trigger()
-    },
-  }))
+export const useRef = <T>(value?: MaybeRef<T>): Ref<T> => useMemo(() => unref(value), [])
