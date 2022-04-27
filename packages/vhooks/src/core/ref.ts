0a802a6ee7bdb440f5ac67243a7820c51db62b81
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-04-27 21:37:51 +0800

    fix(ref): typings

--- packages/vhooks/src/core/ref.ts
+++ packages/vhooks/src/core/ref.ts
@@ -1,9 +1,9 @@
 import type { Ref } from 'vue-demi'
-import { isRef } from 'vue-demi'
-import type { MaybeRef } from '@m9ch/vhooks-types'
+import { unref } from 'vue-demi'
+import type { DeepReadonly, MaybeRef } from '@m9ch/vhooks-types'
 import { useMemo } from './memo'
 
 /**
  * similar to `React.useRef`, but returns `Vue.ref`
  */
-export const useRef = <T>(value?: MaybeRef<T>): Ref<T> => useMemo(() => unref(value), [])
+export const useRef = <T>(value?: MaybeRef<T>): DeepReadonly<Ref<T>> => useMemo<T>(() => unref<T>(value), [])
