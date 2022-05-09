1778cd9ea3ed8128770ae4e499c5fe13b7ee1908
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-05-10 00:59:05 +0800

    fix(ref): refactor ref with customRef

--- packages/vhooks/src/core/ref.ts
+++ packages/vhooks/src/core/ref.ts
@@ -1,9 +1,22 @@
 import type { Ref } from 'vue-demi'
-import { unref } from 'vue-demi'
-import type { DeepReadonly, MaybeRef } from '@m9ch/vhooks-types'
-import { useMemo } from './memo'
+import { customRef, unref } from 'vue-demi'
+import type { MaybeRef } from '@m9ch/vhooks-types'
 
 /**
  * similar to `React.useRef`, but returns `Vue.ref`
  */
-export const useRef = <T>(value?: MaybeRef<T>): DeepReadonly<Ref<T>> => useMemo<T>(() => unref<T>(value), [])
+export const useRef = <T>(initialValue?: MaybeRef<T>): Ref<T> => customRef((track, trigger) => {
+  // auto unwrap ref
+  let current = unref(initialValue)
+
+  return {
+    get: () => {
+      track()
+      return current
+    },
+    set: (newVal) => {
+      trigger()
+      current = newVal
+    },
+  }
+})
