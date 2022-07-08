83f88dc45c0a08ae9e75570392a6ed2f8880c514
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-07-08 11:28:05 +0000

    feat: `useFetch` expose `reload` method

--- packages/vhooks/src/misc/fetch.ts
+++ packages/vhooks/src/misc/fetch.ts
@@ -3,28 +3,35 @@ import { useEffect } from '../core/effect'
 import type { Ref } from 'vue-demi'
 import { readonly, ref, unref } from 'vue-demi'
 
-export const useFetch = <T>(fetch: () => Promise<T>, defaultValue: MaybeRef<T>, deps: any[] = []) => {
+export const useFetch = <T>(fetch: () => Promise<T>, defaultValue?: MaybeRef<T>, deps: any[] = []) => {
   const loading = ref<boolean>(false)
   const error = ref<any>(null)
   const data = ref<T>(unref(defaultValue)) as Ref<T>
 
-  useEffect(() => {
+  const reload = async () => {
+    if (loading.value)
+      return
+
     loading.value = true
-    fetch()
-      .then((response) => {
-        data.value = response
-      })
-      .catch((e) => {
-        error.value = e
-      })
-      .finally(() => {
-        loading.value = false
-      })
+    try {
+      data.value = await fetch()
+    }
+    catch (e) {
+      error.value = e
+    }
+    finally {
+      loading.value = false
+    }
+  }
+
+  useEffect(() => {
+    reload()
   }, [...deps])
 
   return {
     loading: readonly(loading),
     error: readonly(error),
     data: readonly(data),
+    reload,
   }
 }
