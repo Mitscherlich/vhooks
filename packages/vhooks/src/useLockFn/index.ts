126b33b5d6a0973f59f736564e20e453683cd69b
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-07-22 23:50:03 +0800

    feat(useLockFn): more semantic

--- packages/vhooks/src/useLockFn/index.ts
+++ packages/vhooks/src/useLockFn/index.ts
@@ -1,22 +1,18 @@
-import useCallback from '../useCallback'
 import useRef from '../useRef'
 
 export default function useLockFn<P extends any[] = any, V = any>(fn: (...args: P) => Promise<V>) {
   const lockRef = useRef(false)
 
-  return useCallback(
-    async (...args: P) => {
-      if (lockRef.value)
-        return
+  return async (...args: P) => {
+    if (lockRef.value)
+      return
 
-      lockRef.value = true
-      try {
-        return await fn(...args)
-      }
-      finally {
-        lockRef.value = false
-      }
-    },
-    [fn],
-  )
+    lockRef.value = true
+    try {
+      return await fn(...args)
+    }
+    finally {
+      lockRef.value = false
+    }
+  }
 }
