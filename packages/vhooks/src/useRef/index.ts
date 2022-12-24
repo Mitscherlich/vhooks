81174cf5efeee926f1fd1f618c8ec8c97236d63a
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-12-24 22:05:26 +0800

    feat!: useRef

--- packages/vhooks/src/useRef/index.ts
+++ packages/vhooks/src/useRef/index.ts
@@ -1,19 +1,33 @@
-import type { Ref } from 'vue-demi'
 import { customRef, unref } from 'vue-demi'
+import type { Ref } from 'vue-demi'
 import type { MaybeRef } from '@m9ch/vhooks-types'
 
-export default function useRef<T>(initialValue?: MaybeRef<T>): Ref<T> {
+export default function useRef<T>(initValue?: MaybeRef<T>): Ref<T> & { current: T | undefined } {
   // auto unwrap ref
-  let current = unref(initialValue)
+  let current: T | undefined = initValue ? unref(initValue) : undefined
 
-  return customRef((track, trigger) => ({
-    get: () => {
-      track()
-      return current
-    },
-    set: (newVal) => {
-      trigger()
-      current = newVal
-    },
-  }))
+  let _track: Function
+  let _trigger: Function
+  const getter = () => {
+    _track()
+    return initValue ? (current = unref(initValue)) : current
+  }
+  const setter = (newVal: T) => {
+    _trigger()
+    current = newVal
+  }
+
+  const ref = customRef((track, trigger) => {
+    _track = track
+    _trigger = trigger
+    return { get: getter, set: setter }
+  })
+  Object.defineProperty(ref, 'current', {
+    configurable: false,
+    enumerable: true,
+    get: getter,
+    set: setter,
+  })
+
+  return ref as Ref<T> & { current: T | undefined }
 }
