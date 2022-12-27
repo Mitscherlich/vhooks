ab70d1b8454b0615517c440dcbc7b530fa112f2b
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-12-28 00:24:59 +0800

    feat!: useState

--- packages/vhooks/src/useState/index.ts
+++ packages/vhooks/src/useState/index.ts
@@ -1,17 +1,19 @@
 import type {
+  Action,
+  MaybeRef,
   ReturnValue,
 } from '@m9ch/vhooks-types'
-import type { Ref } from 'vue-demi'
 import useReducer from '../useReducer'
 
-export type BasicStateAction<S> = ((S) => S) | S
+export type BasicStateAction<S> = Action<S>
 
 function basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
-  // @ts-expect-error ts doesn't like mixed types
   return typeof action === 'function' ? action(state) : action
 }
 
-function useState<S>(initialState: S): ReturnValue<Ref<S>, BasicStateAction<S>> {
+function useState<S>(initialState: MaybeRef<S>): ReturnValue<S, BasicStateAction<S>>
+function useState<S>(initialState: (s: S) => S): ReturnValue<S, BasicStateAction<S>>
+function useState<S>(initialState: MaybeRef<S> | ((s: S) => S)): ReturnValue<S, BasicStateAction<S>> {
   return useReducer<S, BasicStateAction<S>>(basicStateReducer, initialState)
 }
 
