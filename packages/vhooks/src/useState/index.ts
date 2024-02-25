2d15b51d71ed86f4dcf318a287d2bcb08e59ddf0
Mitscherlich <mitscherlich36@gmail.com>
2024-02-25 23:45:19 +0800

    fix(useState): typings

--- packages/vhooks/src/useState/index.ts
+++ packages/vhooks/src/useState/index.ts
@@ -8,13 +8,13 @@ import useReducer from '../useReducer'
 export type BasicStateAction<S> = Action<S>
 
 function basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
-  return typeof action === 'function' ? action(state) : action
+  return typeof action === 'function' ? (action as (s: S) => S)(state) : action
 }
 
 function useState<S>(initialState: MaybeRef<S>): ReturnValue<S, BasicStateAction<S>>
 function useState<S>(initialState: (s: S) => S): ReturnValue<S, BasicStateAction<S>>
 function useState<S>(initialState: MaybeRef<S> | ((s: S) => S)): ReturnValue<S, BasicStateAction<S>> {
-  return useReducer<S, BasicStateAction<S>>(basicStateReducer, initialState)
+  return useReducer<S, BasicStateAction<S>>(basicStateReducer, initialState as S)
 }
 
 export default useState
